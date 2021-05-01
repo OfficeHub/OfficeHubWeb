ade6492f43fb49d0909245799b5ce6ad1d1ed9a8
zimin <wlals822@naver.com>
2021-05-01 18:44:29 +0900

    중복된 getElementById 함수로 변경
    근데 다른 프로젝트에서는 어떤 식으로 하는지 궁금...

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
-function showNoticePage() {
-    document.getElementById("contentContainer").innerHTML = '<object type="text/html" data="notice.html"></object>';
-}
-
-function showUnreadMailsPage() {
-    document.getElementById("contentContainer").innerHTML = '<object type="text/html" data="unread_mails.html"></object>';
-}
-
-function showUserProfilePage() {
-    document.getElementById("contentContainer").innerHTML = '<object id="userProfileHtml" type="text/html" data="user_profile.html"></object>';
-}
-
-// 맨 처음 로딩시에는 공지사항 화면을 표시해야하는데, 해당 div는 처음의 html이 모두 로딩되었어야 조회 가능
 document.addEventListener("DOMContentLoaded", function() {
     showNoticePage();
 });
+
+function showNoticePage() {
+    findContentContainer().innerHTML =
+        `<object type="text/html" data="notice.html"></object>`;
+}
+
+function showUnreadMailsPage() {
+    findContentContainer().innerHTML =
+        '<object type="text/html" data="unread_mails.html"></object>';
+}
+
+function showUserProfilePage() {
+    findContentContainer().innerHTML =
+        '<object id="userProfileHtml" type="text/html" data="user_profile.html"></object>';
+}
+
+function findContentContainer() {
+    return document.getElementById("contentContainer");
+}
 
 
 var showClassName = "show";
