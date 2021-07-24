cdb6b95a217d0d1849c928141607aa1ab7556091
TAEKWON OH <otw7917@gmail.com>
2021-07-24 20:32:03 +0900

    인덴트 수정

--- src/notice/notice.js
+++ src/notice/notice.js
@@ -1,30 +1,32 @@
-import {noticeTemplate} from './noticeTemplate.js'
+import { noticeTemplate } from './noticeTemplate.js'
 
 document.addEventListener("DOMContentLoaded", () => {
 	fetchNotices()
 })
 
 function fetchNotices() {
-	const noticeurl = 'http://13.125.35.33:8080/notices?offset=0&size=100';
+	let noticeOffset = 0;
+	let noticeSize = 10;
+	const noticeurl = 'http://13.125.35.33:8080/notices?offset=' + noticeOffset + '&size=' + noticeSize;
 	const noticeShow = document.querySelector("#noticeShow");
-	
+
 	fetch(noticeurl, {
-		method:"get",
+		method: "get",
 		headers: {
-		"Content-Type": "application/json"
-		}
+			"Content-Type": "application/json"
+		},
 	}).then((res) => {
 		return res.json();
 	}).then(response => {
 		const noticesArray = response.response.notices;
-		noticesArray.forEach( (item) => {
+		noticesArray.forEach((item) => {
 			noticeShow.innerHTML += noticeTemplate(item);
 		})
-	}).then( () => {
+	}).then(() => {
 		const noticeContainers = document.querySelectorAll(".notice-container");
-		
-		for(let i = 0; i < noticeContainers.length;i++) {
-			noticeContainers[i].addEventListener("click", () =>  {
+		for (let i = 0; i < noticeContainers.length; i++) {
+			noticeContainers[i].addEventListener("click", async () => {
+				await handleNoticeClick();
 				noticeContainers[i].style.backgroundColor = '#ccc';
 				const checkNotice = noticeContainers[i].querySelector(".check");
 				checkNotice.innerText = "확인";
